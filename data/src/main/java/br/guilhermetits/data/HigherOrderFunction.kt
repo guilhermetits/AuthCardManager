bc7b16404255052f5162289c08ac2e7e246aa2eb
Guilherme Titschkoski <guilherme.ti@outlook.com>
2018-04-29 13:01:30 -0300

    Higher order function tests

--- data/src/main/java/br/guilhermetits/data/HigherOrderFunction.kt
+++ data/src/main/java/br/guilhermetits/data/HigherOrderFunction.kt
@@ -2,25 +2,13 @@ package br.guilhermetits.data
 
 import java.lang.Math.pow
 
-typealias Printer = (String) -> Unit
-typealias LazyCalculator = (Operation, (Number) -> Unit) -> Unit
+// Algumas formas de se escrever um callback
+typealias Printer = String.() -> Unit
 
-fun getOperator(operation: Operation, callback: (Number) -> Unit) {
-    return when (operation) {
-        is Sum -> {
-            callback(operation.fisrtVal + operation.secondVal)
-        }
-        is Multiply -> {
-            callback(operation.firstVal * operation.secondVal)
-        }
-        is Divide -> {
-            callback(operation.numerator / operation.denominator)
-        }
-        is Raise -> {
-            callback(pow(operation.number.toDouble(), operation.exponent.toDouble()))
-        }
-    }
-}
+typealias Callback = ((Number) -> Unit)?
+typealias LazyCalculator = (Operation, Callback) -> Unit
+typealias Parser = (String) -> Int
+typealias ParserUnparser = (String) -> (Int) -> (String) -> (Int) -> String
 
 sealed class Operation
 data class Sum(val fisrtVal: Int, val secondVal: Int) : Operation()
@@ -28,23 +16,56 @@ data class Multiply(val firstVal: Int, val secondVal: Int) : Operation()
 data class Divide(val numerator: Int, val denominator: Int) : Operation()
 data class Raise(val number: Int, val exponent: Int) : Operation()
 
-val printer: Printer = { println(it) }
- // method association
+fun getOperator(operation: Operation, callback: Callback) {
+    return when (operation) {
+        is Sum -> {
+            callback?.invoke(operation.fisrtVal + operation.secondVal) ?: Unit
+        }
+        is Multiply -> {
+            callback?.invoke(operation.firstVal * operation.secondVal) ?: Unit
+        }
+        is Divide -> {
+            callback?.invoke(operation.numerator / operation.denominator) ?: Unit
+        }
+        is Raise -> {
+            callback?.invoke(pow(operation.number.toDouble(), operation.exponent.toDouble()))
+                    ?: Unit
+        }
+    }
+}
+
+val printer: Printer = { println(this) }
+// method association
 val lazyCalculator: LazyCalculator = ::getOperator
-val parser = fun(text:String): Int {
-    return Integer.parseInt(text)
+var parser: Parser = fun(text: String): Int {
+    return text.toInt()
+}
+val parserOption: Parser = { text: String ->
+    val uppertext = text.toUpperCase()
+    uppertext.toInt()
+}
+val parserUnparser: ParserUnparser = { str -> { integer -> { str2 -> { integer2 -> integer2.plus(10).toString() } } } }
+
+
+class MyTestClass(private val printer: Printer, private val lazyCalculator: LazyCalculator) {
+    fun calculateAndPrint(op: Operation, calculatorListener: (Number) -> Unit) {
+        lazyCalculator(op) {
+            it.toString().printer()
+            calculatorListener(it)
+        }
+    }
 }
 
 fun main(vararg args: String) {
     MyTestClass(printer, lazyCalculator)
-            .calculateAndPrint(Raise(parser("2"),10))
-}
+            //CLOSURE Callback
+            .calculateAndPrint(Raise(parser("2"), 10)) {
+                parser(it.toString())
+            }
 
-class MyTestClass(private val printer: Printer, private val lazyCalculator: LazyCalculator) {
-    //CLOSURE Callback
-    fun calculateAndPrint(op: Operation) {
-        lazyCalculator(op) {
-            printer(it.toString())
-        }
-    }
+    var result1 = parserUnparser("teste")
+    val result2 = result1(10)
+    val result3 = result2("teste")
+    val result4 = result3(100)
+    val finalResult = result4 == "110"
 }
